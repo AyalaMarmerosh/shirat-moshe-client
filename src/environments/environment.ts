cc9c2c651bc3c2112bb329ac66f1c7260342d40b
ayalaMarmerosh <am0548505474@gmail.com>
2025-01-17 00:50:19 +0200

    TT

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,  // מציין שהסביבה היא פיתוח
-    apiUrl: 'http://api.example.com',
+    apiUrl: 'httpapi.example.com',
     // הוסף משתנים נוספים שצריך עבור פיתוח
   };
   
